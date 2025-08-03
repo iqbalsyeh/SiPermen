8740271878e273a9d6220eb43dd77e76eb807203
iqbalsyeh <iqbalsyehmaulana1@gmail.com>
2025-08-03 18:49:11 +0700

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,4 +1,5 @@
 # utils/ocr.py
+from pdf2image import convert_from_path
 
 import os
 import fitz  # PyMuPDF
@@ -6,22 +7,21 @@ import pytesseract
 from PIL import Image
 
 
-def ocr_pdf_to_text(pdf_path: str, output_dir="hasil_teks") -> str:
-    text_result = ""
+def ocr_pdf_to_text(pdf_path: str, output_dir: str = "hasil_teks") -> str:
+        
+    images = convert_from_path(pdf_path)
+    text = ""
+    for img in images:
+        gray = img.convert("L")
+        text += pytesseract.image_to_string(gray, lang="ind") + "\n"
 
-    with fitz.open(pdf_path) as doc:
-        for page in doc:
-            pix = page.get_pixmap(dpi=300)
-            img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-            text = pytesseract.image_to_string(img)
-            text_result += text + "\n\n"
-
+    # Simpan hasil ke .txt
     os.makedirs(output_dir, exist_ok=True)
-    
-    base_name = os.path.basename(pdf_path).replace(".pdf", ".txt")
-    txt_path = os.path.join(output_dir, base_name)
+    nama_file = os.path.basename(pdf_path).replace(".pdf", ".txt")
+    with open(os.path.join(output_dir, nama_file), "w", encoding="utf-8") as f:
+        f.write(text)
 
-    with open(txt_path, "w", encoding="utf-8") as f:
-        f.write(text_result)
+    return text
 
-    return text_result
+   
+
